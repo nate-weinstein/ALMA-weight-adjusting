dedba57e922056aee5c0d2122a143d874084f2c3
Nate Weinstein <nweinstein@wesleyan.edu>
2025-11-19 22:53:45 -0500

    testing

--- new_var_vis.py
+++ new_var_vis.py
@@ -1,4 +1,4 @@
-def new_var_vis(file,collapse=False,realimag=False):
+def new_var_vis(collapse=False,realimag=False):
     ''' Calculate the weight based on the variance in a visibility map at each u,v point and each channel. The codes estimate the variance among the 50 closest uv-points in a limited range in uv-space. 
 
     :param file:
@@ -12,21 +12,22 @@ def new_var_vis(file,collapse=False,realimag=False):
     ''' 
     from astropy.io import fits
     import numpy as np
-    
+    file = input("Input file path: ")
     fits_file = fits.open(file)
-    u,v = fits_file[0].data['UU'],fits_file[0].data['VV']
-    freq0 = fits_file[0].header['crval4']
-    klam = freq0/1e3
+    #u,v = fits_file[0].data['UU'],fits_file[0].data['VV']
+    #freq0 = fits_file[0].header['crval4']
+    #klam = freq0/1e3
 
     vis = (fits_file[0].data['data']).squeeze()
     fits_file.close()
 
     import time
     start=time.time()
-    weight = np.zeros((u.size(),(vis.shape)[1],2))
+    #weight = np.zeros((u.size(),(vis.shape)[1],2))
     chi = 0
     for i in range():
         chi += ((vis[:,i,0,0]**2)*vis[:,i,0,2]).sum() + ((vis[:,i,0,1]**2)*vis[:,i,0,2]).sum() + ((vis[:,i,1,0]**2)*vis[:,i,1,2]).sum() + ((vis[:,i,1,1]**2)*vis[:,i,1,2]).sum()
-    red_chi = chi/(len(vis[:])*4)
-    print('Reduced chi square = ' + str(red_chi))
+    red_chi = chi/(len(vis[:])*2*2*vis[0,:,0,0].size())
+    print('Chi square = ',str(chi))
+    print('Reduced chi square = ',str(red_chi))
     print('Elapsed time (hrs): ',(time.time()-start)/3600.)
